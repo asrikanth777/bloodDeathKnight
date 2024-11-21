9325e560972f327960e04f3de6d0df859a62dd78
Aditya Srikanth <as140@illinois.edu>
2024-11-21 01:54:56 -0600

    started on fetch_character

--- fetch_character.cpp
+++ fetch_character.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
 #include <string>
 #include <curl/curl.h>
+#include <fstream>
 
-using namespace std;
+
+int main() {
+    using namespace std;
+
+    // recreating variables from post_request.cpp used to pull accesstoken data and write into file
+    string accesstoken;
+    ifstream tokenFile("accessTOKEN.txt");
+
+
+    // if tokenfile is available, it takes the text in the file and writes it into accesstoken
+    // the error code is self explanatory
+    if (tokenFile.is_open()) {
+        getline(tokenFile, accesstoken);
+        tokenFile.close();
+        cout << "Access token loaded: " << accesstoken << endl;
+    } else {
+        cerr << "Failed to open accessTOKEN.txt to read for token" << endl;
+    }
+
+    return 0;
+}
